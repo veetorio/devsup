aee64517a1c98d20b097ccf78b13352027bc0abe
vee/torio <134203950+veetorio@users.noreply.github.com>
2023-11-13 21:25:32 -0300

    Add files via upload

---
+++ projetoSpring/src/test/java/com/oxigeny/projetoSpring/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oxigeny.projetoSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
